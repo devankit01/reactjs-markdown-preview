50d13c9ceb1edcba69ca2c1b417d17cd16c21de9
devankit01 <devankitgupta01@gmail.com>
2020-09-25 03:00:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import { marked } from 'marked';
+let marked = require("marked");
 
 function App() {
+  const [output, setoutput] = useState("");
+  const handle = (e) => {
+    console.log(e.target.value);
+    setoutput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
+
+      <div className="parent">
+        <div className="Input">
+          <h2>Input</h2>
+          <textarea onChange={handle} value={output} placeholder="Type in Markdown format "></textarea>
+        </div>
+        <div className="Output">
+        <h2>Output</h2>
+
+          <span
+            dangerouslySetInnerHTML={{
+              __html: marked(output, { sanitize: true }),
+            }}
+          ></span>
+        </div>
+      </div>
     </div>
   );
 }
